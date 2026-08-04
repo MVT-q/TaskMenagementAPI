cc2bd3931520297d5f0090df766d0fe6026de622
Yehor Radykop <egorradikop@gmail.com>
2026-08-04 12:57:36 +0200

    Configure Entity Framework Core

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using TaskMenagementAPI.Data;
 
 namespace TaskMenagementAPI
 {
@@ -8,8 +10,15 @@ namespace TaskMenagementAPI
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddControllers();
+
             builder.Services.AddEndpointsApiExplorer();
+
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
 
